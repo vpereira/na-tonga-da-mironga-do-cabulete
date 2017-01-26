f00af97de426accbebe4bab3bfe2a749854b8d94
Victor Pereira <vpereirabr@gmail.com>
2017-01-26 15:38:53 +0100

    refactored code and fixed typo

--- tiw/app/helpers/main.rb
+++ tiw/app/helpers/main.rb
@@ -1,7 +1,7 @@
 def breadcrumb_codestream_package(codestream, package)
-  "#{link_to(codestream,"/codestreams/#{codestream}/")} > #{link_to(package,"/codestreams/#{codestream}/packages/#{package}/")}"
+   breadcrumb_codestream(codestream) + " > #{link_to(package,"/codestreams/#{codestream}/packages/#{package}/")}"
 end
 
-def breadcrum_codestream(codestream)
+def breadcrumb_codestream(codestream)
   "#{link_to(codestream,"/codestreams/#{codestream}/")}"
 end
