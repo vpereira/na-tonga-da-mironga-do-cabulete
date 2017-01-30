5694f47d0763fc046cb95f8088a0654933e28de1
Victor Pereira <vpereira@suse.com>
2017-01-30 09:33:02 +0000

    persistence in the db / wait until postgres is up

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@ set -x
 cd /tiw
 rake.ruby2.2 ar:create
 rake.ruby2.2 ar:migrate
+# wait until db really started (not just container but service as well)
+while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/db/5432' >/dev/null 2>/dev/null; do sleep 0.1; done
 bin/padrino s -h "0.0.0.0" -p 3000
